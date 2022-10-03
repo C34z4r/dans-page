ec1c6fe1a93150e1ef4677afd68283c132f4149c
Daniel Convers <danielgimnasio2003@hotmail.com>
2022-10-03 11:15:54 -0500

    Agregado fondo, estrellas, y otros modelos más con ThreeJS

--- danspage/main.js
+++ danspage/main.js
@@ -39,6 +39,26 @@ scene.add(torus);
 
 animate();
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff
+  })
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x,y,z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+
+
 function animate(){
   requestAnimationFrame(animate);
 
@@ -50,3 +70,31 @@ function animate(){
 
   renderer.render(scene, camera);
 }
+
+//avatar
+
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const normal = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  
+  new THREE.MeshBasicMaterial({
+    map:normalTexture
+  })
+);
+
+scene.add(normal);
+
+//Moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({
+    map:moonTexture,
+    normalMap: normalTexture
+  })
+);
+
+scene.add(moon);
